230fee2313d606e486bec1f0b4c14f3e7180f4a4
pmhxhsj <minho7952@naver.com>
2021-11-28 15:28:14 +0900

    테스트 통과 및 모든 기능 요구사항 구현 next_commit: 다른 요구사항 일치 확인

--- src/index.js
+++ src/index.js
@@ -7,18 +7,31 @@
 // 동작
 // (ㅇ) MissionUtils 라이브러리의 랜덤 사용하여 랜덤 숫자 3개 생성한다
 // (ㅇ) play메서드에서 사용자가 입력한 수와 컴퓨터의 수를 비교해  스트라이크, 볼, 낫싱 문자열로 리턴한다
-// () 사용자가 모두 맞히면(=3스트라이크) 게임이 종료되고, 재시작 버튼이 노출된다.
-// () 재시작 버튼을 통해 다시 시작할 수 있다.
+// (ㅇ) 사용자가 모두 맞히면(=3스트라이크) 게임이 종료되고, 재시작 버튼이 노출된다.
+// (ㅇ) 재시작 버튼을 통해 다시 시작할 수 있다.
 
 // 출력
 // (ㅇ) 입력한 세 숫자에 따라 볼, 스트라이크, 낫싱 출력(볼이 스트라이크보다 앞에 출력)
-// () 정답 화면 및 재시작 버튼 출력
+// (ㅇ) 정답 화면 및 재시작 버튼 출력
+function computerRandomNumber() {
+    let uniqueValueStack = [];
 
-const COMPUTER_RANDOM_NUMBER = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3).join('');
+    while (uniqueValueStack.length < 3) {
+        let value = MissionUtils.Random.pickNumberInRange(1, 9);
+        if (!uniqueValueStack.includes(value)) {
+            uniqueValueStack.push(value);
+        }
+    }
+
+    return uniqueValueStack.join('');
+}
+
+const computerNumber = computerRandomNumber();
 
 export default function BaseballGame() {
+
     this.play = function (computerInputNumbers, userInputNumbers) {
-        computerInputNumbers = COMPUTER_RANDOM_NUMBER;
+        computerInputNumbers = computerNumber;
         userInputNumbers = document.querySelector('#user-input').value.split('');
         let [strikeCount, ballCount] = [0, 0];
 
@@ -35,7 +48,6 @@ export default function BaseballGame() {
     };
 }
 
-console.log(COMPUTER_RANDOM_NUMBER)
 
 import currency from './index.js';
 
@@ -50,7 +62,7 @@ function gameResultPrint(strikeNum, ballNum) {
 }
 
 function inputException(value) {
-    if (value.length !== 3 || isNaN(value) || value.includes(0)) {
+    if (value.length !== 3 || isNaN(value) || value.includes(0) || value.includes(' ')) {
         return true;
     } else if ([...new Set(value.split(''))].length !== value.length) {
         return true;
